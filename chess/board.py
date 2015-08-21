99e9698937e5dd56bf7dedbd18e31cfb863c7a52
Xavi Francisco <xavi.francisco@gmail.com>
2015-08-21 10:59:18 +0200

    Include the logic of the program.
    This module is used to compute all the possible permutations of pieces inside a board. Thus, two modules are defined: Board and BoardPermutation.
    
    Board holds the current size of a Board and the included pieces.
    
    BoardPermutation is where the calculations are done.

---
+++ chess/board.py
@@ -0,0 +1,124 @@
+from __future__ import print_function
+
+import copy
+
+from pip.util import get_terminal_size
+
+from pieces import create_piece
+
+
+class Board(object):
+    def __init__(self, rows, columns, pieces=None):
+        self.rows = rows
+        self.columns = columns
+        self.pieces = pieces or []
+
+    def get_row_column(self, offset):
+        return divmod(offset, self.columns)
+
+    def get_offset(self, row, column):
+        return row * self.columns + column
+
+    def get_piece_offsets(self):
+        return map(lambda x: self.get_offset(x.x, x.y),
+                   filter(lambda x: x.is_placed(), self.pieces))
+
+    def is_safe(self, piece):
+        for p in self.pieces:
+            if p.is_attacking(piece) or piece.is_attacking(p):
+                return False
+        return True
+
+    def __str__(self):
+        data = [['.' for _ in range(self.columns)] for _ in range(self.rows)]
+        for piece in self.pieces:
+            data[piece.x][piece.y] = '{}'.format(piece)
+        return '\n'.join(
+            [''.join(['{}'.format(item) for item in row]) for row in data])
+
+    def place_piece(self, piece, offset):
+        if offset >= self.rows * self.columns:
+            raise ValueError("Out of Bounds")
+        for next_offset in range(offset, self.rows * self.columns):
+            row, column = self.get_row_column(next_offset)
+            if self.is_safe(create_piece(piece.name, row, column)):
+                piece.x, piece.y = row, column
+                self.pieces.append(piece)
+                return next_offset
+        raise ValueError("Out of Bounds")
+
+    def remove_piece(self, piece):
+        self.pieces.remove(piece)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return (self.rows == other.rows and
+                self.columns == other.columns and
+                self.pieces == other.pieces)
+
+    def __copy__(self):
+        return Board(self.rows, self.columns, list(self.pieces))
+
+
+class BoardPermutation(object):
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+
+    def place_pieces(self, board_list, board, pieces, index=0, offset=0):
+        if index == len(pieces):
+            board_list.append(board)
+        else:
+            current_offset = offset
+            piece = create_piece(pieces[index])
+            while current_offset < (board.rows * board.columns):
+                try:
+                    used_offset = board.place_piece(piece, current_offset)
+                except ValueError:
+                    break
+                self.place_pieces(board_list, copy.deepcopy(board), pieces,
+                                  index+1, used_offset+1)
+                board.remove_piece(piece)
+                current_offset = used_offset + 1
+
+    def build_boards(self, list_of_piece_names):
+        permutations = []
+        board_list = set()
+        self.build_permutations(permutations, [], list_of_piece_names)
+        for pos, permutation in enumerate(permutations):
+            boards = self.build_board(permutation)
+            board_list.update(set(boards))
+        return board_list
+
+    def build_board(self, permutation):
+        """
+
+        :param permutation: :type list
+        :return:
+        """
+        boards = []
+        board = Board(self.rows, self.columns)
+        self.place_pieces(boards, board, permutation, 0, 0)
+        return boards
+
+    def build_permutations(self, permutations, final, initial):
+        """
+        Create all the possible permutations of a list of pieces.
+        :type permutations: list
+        :type final: list
+        :type initial: list
+        """
+        if not initial:
+            if final not in permutations:
+                permutations.append(final)
+        else:
+            for position, _ in enumerate(initial):
+                initial_copy = list(initial)
+                final_copy = list(final)
+                final_copy.append(initial_copy.pop(position))
+                self.build_permutations(permutations, final_copy, initial_copy)
+
+    def __str__(self):
+        width, _ = get_terminal_size()
